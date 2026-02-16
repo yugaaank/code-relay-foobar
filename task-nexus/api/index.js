69dc4e5279d3744e5d006edb9c3f688c4285eba9
yugaaank <yugaankrathore0@gmail.com>
2026-02-16 16:15:00 +0530

    changes to serverlss

--- task-nexus/api/index.js
+++ task-nexus/api/index.js
@@ -1,4 +1,5 @@
-const app = require("../server/server");
+const path = require("path");
+const app = require(path.join(__dirname, "../server/server"));
 
 // Vercel serverless function entrypoint
 module.exports = app;
